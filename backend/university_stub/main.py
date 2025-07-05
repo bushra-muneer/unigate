a67681323586c2268e3bd00c9af906eaf391933a
Bushra Muneer <bushraahmed302@gmail.com>
2025-07-05 05:09:37 +0200

    Fix university stub DB session

--- backend/university_stub/main.py
+++ backend/university_stub/main.py
@@ -4,7 +4,7 @@ from datetime import date
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlmodel import Session, select
-from unigate.core.database import get_session
+from unigate.core.database import get_unistub_session
 from .models import Course, Exam, ExamResult, Student
 
 app = FastAPI(title="University Stub")
@@ -15,14 +15,14 @@ app = FastAPI(title="University Stub")
 
 
 @app.get("/stub/students/{number}", response_model=Student)
-def get_student(number: int, session: Session = Depends(get_session)) -> Student:
+def get_student(number: int, session: Session = Depends(get_unistub_session)) -> Student:
     student = session.exec(select(Student).where(Student.number == number)).first()
     if not student:
         raise HTTPException(status_code=404, detail="Student not found")
     return student
 
 @app.get("/stub/courses", response_model=list[Course])
-def list_courses(session: Session = Depends(get_session)) -> Sequence[Course]:
+def list_courses(session: Session = Depends(get_unistub_session)) -> Sequence[Course]:
     """List all courses."""
     try:
         courses = session.exec(select(Course)).all()
@@ -33,7 +33,7 @@ def list_courses(session: Session = Depends(get_session)) -> Sequence[Course]:
     return courses
 
 @app.get("/stub/courses/{name}", response_model=Course)
-def get_course(name: str, session: Session = Depends(get_session)) -> Course:
+def get_course(name: str, session: Session = Depends(get_unistub_session)) -> Course:
     course = session.exec(select(Course).where(Course.name == name)).first()
     if not course:
         raise HTTPException(status_code=404, detail="Course not found")
@@ -43,7 +43,7 @@ def get_course(name: str, session: Session = Depends(get_session)) -> Course:
 def query_results(
     course: str,
     exam_date: date,
-    session: Session = Depends(get_session),
+    session: Session = Depends(get_unistub_session),
 ) -> list[ExamResult]:
     exam = session.exec(
         select(Exam)
@@ -58,7 +58,7 @@ def query_results(
 
 @app.get("/stub/exams/{course}/{exam_date}", response_model=list[ExamResult])
 def get_exam_result(
-    course: str, exam_date: str, session: Session = Depends(get_session)
+    course: str, exam_date: str, session: Session = Depends(get_unistub_session)
 ) -> list[ExamResult]:
     try:
         parsed_date = date.fromisoformat(exam_date)
